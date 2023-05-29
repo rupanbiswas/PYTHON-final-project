b93612e7c99b56f5dfaa12f6e7be598128e0a09f
rupan <rupanbiswas18rb@gmail.com>
2023-05-30 02:18:22 +0530

    initial commit

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Choice, Question
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
+    extra = 5
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+class ChoiceInline(admin.StackedInline):
+    model = Choice
     extra = 5
 
 
@@ -21,10 +28,18 @@ class CourseAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+# class QuestionAdmin(admin.ModelAdmin):
+#     inlines = [QuestionInline]
+
+# class ChoiceAdmin(admin.ModelAdmin):
+#     inlines = [ChoiceInline]
+
 
 # <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question)
+admin.site.register(Choice)
 admin.site.register(Instructor)
 admin.site.register(Learner)
--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -54,6 +54,7 @@ class Learner(models.Model):
 
 # Course model
 class Course(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(null=False, max_length=30, default='online course')
     image = models.ImageField(upload_to='course_images/')
     description = models.CharField(max_length=1000)
@@ -101,19 +102,22 @@ class Enrollment(models.Model):
     # Has a grade point for each question
     # Has question content
     # Other fields and methods you would like to design
-#class Question(models.Model):
-    # Foreign key to lesson
-    # question text
-    # question grade/mark
+class Question(models.Model):
+    # course = models.ForeignKey(Course,default=1, on_delete=models.CASCADE,)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    text = models.TextField()
+    point = models.IntegerField(default=0)
+    # course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
 
     # <HINT> A sample model method to calculate if learner get the score of the question
-    #def is_get_score(self, selected_ids):
-    #    all_answers = self.choice_set.filter(is_correct=True).count()
-    #    selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
-    #    if all_answers == selected_correct:
-    #        return True
-    #    else:
-    #        return False
+    def is_get_score(self, selected_ids):
+       all_answers = self.choice_set.filter(is_correct=True).count()
+       selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+       if all_answers == selected_correct:
+           return True
+       else:
+           return False
 
 
 #  <HINT> Create a Choice Model with:
@@ -122,13 +126,17 @@ class Enrollment(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
+class Choice(models.Model):
+    # course = models.ForeignKey(Course,default=1, on_delete=models.CASCADE,)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.TextField()
+    is_correct = models.BooleanField()
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+   enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+   choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
